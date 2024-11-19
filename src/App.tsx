525955cdcb40b73b6bfcef30231728032ec05deb
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-11-19 14:45:16 +0200

    Added tailwindcss to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemonImg, setPokemonImg] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+      const pokemonObj = await res.json();
+      // let pokemonImg = pokemonObj;
+      console.log(pokemonObj.sprites["front_default"]);
+      setPokemonImg(pokemonObj.sprites["front_default"]);
+    }
+    fetchData();
+  }, []);
+
+  if (pokemonImg === null) {
+    return <h1>No pokemon img</h1>;
+  }
+  console.log(pokemonImg);
+  return <img src={pokemonImg} alt="Pokemon img" />;
 }
 
 export default App;
