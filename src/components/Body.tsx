33d28f37e862ae7f1d0ab62dc155a266f3b8780a
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-11-20 11:54:08 +0200

    Cards can now toggle overlay when expanded

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 function Body() {
   const [pokemonObj, setPokemonObj] = useState<PokemonObj | null>(null);
+  const [isOverlayOn, setIsOverlayOn] = useState(false);
 
   useEffect(() => {
     async function loadPokemon() {
@@ -14,16 +15,33 @@ function Body() {
     loadPokemon();
   }, []);
 
+  const toggleOverlayCallback: (arg: boolean) => void = (setOverlay) => {
+    setIsOverlayOn(setOverlay);
+  };
+
   if (pokemonObj === null) {
     return <h1>Loading...</h1>;
   }
 
   return (
-    <div className="h-fit w-full bg-amber-200 p-10 grid justify-items-center gap-y-10 xl:grid-cols-5 md:grid-cols-3 ">
-      {Array.from({ length: 15 }).map((_, i) => (
-        <PokemonCard pokemonObj={pokemonObj} />
-      ))}
-    </div>
+    <>
+      <div
+        className={
+          "fixed inset-0 bg-black transition-opacity duration-1000 " +
+          (isOverlayOn
+            ? "opacity-80 pointer-events-auto"
+            : "opacity-0 pointer-events-none")
+        }
+      ></div>
+      <div className="h-fit w-full bg-amber-200 p-10 grid justify-items-center gap-y-10 xl:grid-cols-5 md:grid-cols-3 ">
+        {Array.from({ length: 15 }).map((_, i) => (
+          <PokemonCard
+            pokemonObj={pokemonObj}
+            toggleOverlayCallback={toggleOverlayCallback}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
 import { PokemonObj } from "../types";
 
 interface PokemonCardProps {
   pokemonObj: PokemonObj;
+  toggleOverlayCallback: (setOverlay: boolean) => void;
 }
 
-function PokemonCard({ pokemonObj }: PokemonCardProps) {
+function PokemonCard({ pokemonObj, toggleOverlayCallback }: PokemonCardProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function onCardClick() {
+    toggleOverlayCallback(!isExpanded);
+    setIsExpanded(!isExpanded);
+  }
+
   const pokemonName = pokemonObj.name;
   const pokemonImg = pokemonObj.sprites.front_default;
 
   return (
-    <div className="h-fit w-52 rounded bg-red-500 p-3">
+    <div
+      onClick={onCardClick}
+      className={
+        "rounded bg-red-500 " +
+        (isExpanded ? "absolute w-8/12 h-4/6" : "h-fit w-52 p-3")
+      }
+    >
       <img
         src={pokemonImg}
         alt="Pokemon img"
-        className="w-full bg-white rounded-full"
+        className={
+          "w-full bg-white rounded-full " + (isExpanded ? "hidden" : "")
+        }
       />
-      <span className="block w-fit mx-auto text-xl mt-2">{pokemonName}</span>
+      <span className={"block w-fit mx-auto text-xl mt-2"}>{pokemonName}</span>
     </div>
   );
 }
